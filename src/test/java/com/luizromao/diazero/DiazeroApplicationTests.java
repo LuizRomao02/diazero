fbdb904aaed39a3e019690e4d2ddc1f589aa8ff5
LuizRomao02 <luizromao7@outlook.com>
2024-06-27 12:14:55 -0300

    First Commit

---
+++ src/test/java/com/luizromao/diazero/DiazeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizromao.diazero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiazeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
